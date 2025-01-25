0e335d5f3edaee07784c5865983aa878e93fdd21
Dmytro Chaika <djseagull90@gmail.com>
2025-01-25 21:14:57 +0100

    format code

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,25 +1,18 @@
-import Contact from "../Contact/Contact"
-import s from "./ContactList.module.css"
-
-
+import Contact from "../Contact/Contact";
+import s from "./ContactList.module.css";
 
 const ContactList = ({ contacts, onDelete }) => {
+  if (contacts.length === 0) {
+    return <p>Список контактів порожній.</p>;
+  }
 
-if (contacts.length === 0) {
-        return <p>Список контактів порожній.</p>;
-    }
-  
-    return (
-        <div className={s.contactsList}>
-            {contacts.map((contact, idx) => (
-               
-                <Contact key={idx} contactItem={contact} onDelete={onDelete}   />
-                 
-            ))}
-           
-        </div>
-        
-    )
- }
+  return (
+    <div className={s.contactsList}>
+      {contacts.map((contact, idx) => (
+        <Contact key={idx} contactItem={contact} onDelete={onDelete} />
+      ))}
+    </div>
+  );
+};
 
-export default ContactList
+export default ContactList;
--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,15 +1,19 @@
-import s from "./SearchBox.module.css"
+import s from "./SearchBox.module.css";
 const SearchBox = ({ searchTerm, onSearchChange }) => {
-  
-  return (
-<div>
-     <div>
-      <input onChange={onSearchChange}  value={searchTerm} placeholder="пошук" className={s.inputSearchIcon} type="text" name="searchInput"  />
-     
+  return (
+    <div>
+      <div>
+        <input
+          onChange={onSearchChange}
+          value={searchTerm}
+          placeholder="пошук"
+          className={s.inputSearchIcon}
+          type="text"
+          name="searchInput"
+        />
+      </div>
     </div>
-              
-</div>
-  );
+  );
 };
 
-export default SearchBox 
+export default SearchBox;
